ab0f3c14a68a374e7dce85c25be21851f432eeb3
egipsinargo <egipsinargo123@gmail.com>
2025-02-16 21:48:06 +0700

    add route to tasks page

--- src/router.jsx
+++ src/router.jsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter, Navigate } from "react-router-dom";
 import AfterLoginNav from "./layouts/AfterLoginNav";
 import Dashboard from "./pages/Dashboard";
+import Tasks from "./pages/Tasks";
 
 export const router = createBrowserRouter([
   //   {
@@ -14,6 +15,7 @@ export const router = createBrowserRouter([
     children: [
       { index: true, element: <Navigate to="/dashboard" /> },
       { path: "/dashboard", element: <Dashboard /> },
+      { path: "/tasks", element: <Tasks /> },
     ],
   },
 ]);
