d2228b3ea009ee9f069d256a5d55c24092b416d7
egipsinargo <egipsinargo123@gmail.com>
2025-02-20 22:49:57 +0700

    route to auth

--- src/router.jsx
+++ src/router.jsx
@@ -1,28 +1,30 @@
 import { createBrowserRouter, Navigate } from "react-router-dom";
 import AfterLoginNav from "./layouts/AfterLoginNav";
 import Dashboard from "./pages/Dashboard";
-import Login from "./pages/Login";
+import Auth from "./pages/Auth";
+import HomeNav from "./layouts/HomeNav";
+import Home from "./pages/Home";
+// import Login from "./pages/Login";
 
 export const router = createBrowserRouter([
-  //   {
-  //     path: "/",
-  //     element: <HomeNav />,
-  //     children: [{ index: true, element: <Home /> }],
-  //   },
   {
     path: "/",
-    element: <AfterLoginNav />,
+    element: <HomeNav />,
     children: [
-      { index: true, element: <Navigate to="/dashboard" /> },
-      { path: "/dashboard", element: <Dashboard /> },
+      { index: true, element: <Home /> },
+      { path: "/auth", element: <Auth /> },
     ],
   },
   {
-    path: "/login",
-    element: <Login />,
-    // children: [
-    //   { index: true, element: <Navigate to="/dashboard" /> },
-    //   { path: "/dashboard", element: <Dashboard /> },
-    // ],
+    path: "/success-login",
+    element: <AfterLoginNav />,
+    children: [
+      { index: true, element: <Navigate to="/dashboard" /> },
+      { path: "/success-login/dashboard", element: <Dashboard /> },
+    ],
   },
+  // {
+  //   path: "/login",
+  //   element: <Login />,
+  // },
 ]);
