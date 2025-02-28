3fc9b2a160df62f1c7176f31d8ccb3d922e583ad
egipsinargo <egipsinargo123@gmail.com>
2025-02-28 09:43:51 +0700

    merging both changes

--- client/src/router.jsx
+++ client/src/router.jsx
@@ -1,14 +1,11 @@
 import { createBrowserRouter, Navigate } from "react-router-dom";
 import AfterLoginNav from "./layouts/AfterLoginNav";
 import Dashboard from "./pages/Dashboard";
-<<<<<<< HEAD:src/router.jsx
 import Settings from "./pages/Settings";
-=======
 import Calendar from "./pages/Calendar";
 import Projects from "./pages/Projects";
 import AddTask from "./pages/AddTask";
 import EditTask from "./pages/EditTask";
->>>>>>> feature:client/src/router.jsx
 
 export const router = createBrowserRouter([
   //   {
@@ -21,10 +18,8 @@ export const router = createBrowserRouter([
     element: <AfterLoginNav />,
     children: [
       { index: true, element: <Navigate to="/dashboard" /> },
-<<<<<<< HEAD:src/router.jsx
       { path: "dashboard", element: <Dashboard /> },
       { path: "settings", element: <Settings /> },
-=======
 
       { path: "calendar", element: <Calendar /> },
       {
@@ -33,7 +28,6 @@ export const router = createBrowserRouter([
       },
       { path: "add-task", element: <AddTask /> },
       { path: "edit-task/:id", element: <EditTask /> },
->>>>>>> feature:client/src/router.jsx
     ],
   },
 ]);
